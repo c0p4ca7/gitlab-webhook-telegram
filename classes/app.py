f5a28580c7a0cec646eaf2117f3722789654c7ea
Vladislav Litvinov <vlitvinov@solit-clouds.ru>
2022-12-19 16:33:02 +0400

    feat: filter hook_url_path

--- classes/app.py
+++ classes/app.py
@@ -81,7 +81,7 @@ def get_RequestHandler(bot: Bot, context: CallbackContext) -> RequestHandlerType
             Handler for POST requests
             """
             if self.path != hook_url_path:
-                logging.error("URL path: " + self.path + " - is not valid..")
+                logging.error("URL path: " + self.path + " - is not valid.")
                 self._set_headers(403)
             token = self.headers["X-Gitlab-Token"]
             if self.context.is_authorized_project(token):
